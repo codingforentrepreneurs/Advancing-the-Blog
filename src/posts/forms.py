7f5501bd71409119c08194787d0baee57f0e47cb
CFE <hello@teamcfe.com>
2016-03-22 13:50:48 -0700

    8 - Dynamic Preview of Form Data

--- src/posts/forms.py
+++ src/posts/forms.py
@@ -6,7 +6,7 @@ from .models import Post
 
 
 class PostForm(forms.ModelForm):
-    content = forms.CharField(widget=PagedownWidget)
+    content = forms.CharField(widget=PagedownWidget(show_preview=False))
     publish = forms.DateField(widget=forms.SelectDateWidget)
     class Meta:
         model = Post
