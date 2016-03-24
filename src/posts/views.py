4947a5473e5d8e333eb4ed27678fe6365c2bdbbf
CFE <hello@teamcfe.com>
2016-03-24 15:30:51 -0700

    13 - Django Generic Foreign Keys Part 3

--- src/posts/views.py
+++ src/posts/views.py
@@ -9,14 +9,18 @@ except:
     pass
 
 from django.contrib import messages
+from django.contrib.contenttypes.models import ContentType
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 from django.db.models import Q
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
+from comments.models import Comment
 from .forms import PostForm
 from .models import Post
+
 
 def post_create(request):
 	if not request.user.is_staff or not request.user.is_superuser:
@@ -41,10 +45,16 @@ def post_detail(request, slug=None):
 		if not request.user.is_staff or not request.user.is_superuser:
 			raise Http404
 	share_string = quote_plus(instance.content)
+	content_type = ContentType.objects.get_for_model(Post)
+	obj_id = instance.id
+	#Post.objects.get(id=instance.id)
+	comments = Comment.objects.filter(content_type=content_type, object_id= obj_id)
+
 	context = {
 		"title": instance.title,
 		"instance": instance,
 		"share_string": share_string,
+		"comments": comments,
 	}
 	return render(request, "post_detail.html", context)
 
