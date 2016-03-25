d5b050656c5b5e5c27c57a716029927cde767c90
CFE <hello@teamcfe.com>
2016-03-25 15:03:42 -0700

    16 - Reply to Comments

--- src/comments/models.py
+++ src/comments/models.py
@@ -8,10 +8,14 @@ from django.db import models
 
 
 class CommentManager(models.Manager):
+    def all(self):
+        qs = super(CommentManager, self).filter(parent=None)
+        return qs
+
     def filter_by_instance(self, instance):
         content_type = ContentType.objects.get_for_model(instance.__class__)
         obj_id = instance.id
-        qs = super(CommentManager, self).filter(content_type=content_type, object_id= obj_id)
+        qs = super(CommentManager, self).filter(content_type=content_type, object_id= obj_id).filter(parent=None)
         return qs
 
 class Comment(models.Model):
@@ -19,12 +23,15 @@ class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
-
+    parent      = models.ForeignKey("self", null=True, blank=True)
 
     content     = models.TextField()
     timestamp   = models.DateTimeField(auto_now_add=True)
 
     objects = CommentManager()
+
+    class Meta:
+        ordering = ['-timestamp']
 
 
     def __unicode__(self):  
@@ -32,3 +39,15 @@ class Comment(models.Model):
 
     def __str__(self):
         return str(self.user.username)
+
+    def children(self): #replies
+        return Comment.objects.filter(parent=self)
+
+    @property
+    def is_parent(self):
+        if self.parent is not None:
+            return False
+        return True
+
+
+
--- src/posts/views.py
+++ src/posts/views.py
@@ -57,12 +57,26 @@ def post_detail(request, slug=None):
 		content_type = ContentType.objects.get(model=c_type)
 		obj_id = form.cleaned_data.get('object_id')
 		content_data = form.cleaned_data.get("content")
+		parent_obj = None
+		try:
+			parent_id = int(request.POST.get("parent_id"))
+		except:
+			parent_id = None
+
+		if parent_id:
+			parent_qs = Comment.objects.filter(id=parent_id)
+			if parent_qs.exists() and parent_qs.count() == 1:
+				parent_obj = parent_qs.first()
+
+
 		new_comment, created = Comment.objects.get_or_create(
 							user = request.user,
 							content_type= content_type,
 							object_id = obj_id,
-							content = content_data
+							content = content_data,
+							parent = parent_obj,
 						)
+		return HttpResponseRedirect(new_comment.content_object.get_absolute_url())
 
 
 	comments = instance.comments
